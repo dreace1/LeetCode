2017152fd9a4153573e791445e8c2c135bd776cf
Hannes <hannesro27@gmail.com>
2023-06-28 11:23:19 +0200

    solved TwoSum problem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        TwoSum twoSumObj = new TwoSum();
+        System.out.println(Arrays.toString(twoSumObj.twoSum(new int[]{2, 7, 11, 15}, 9)));
+
 
     }
 }
--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -1,5 +1,25 @@
-public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class TwoSum {
+/*
+    https://leetcode.com/problems/two-sum/submissions/981493186/
+*/
+    public int[] twoSum(int[] nums, int target) {
+        List<Integer> output = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    output.add(i);
+                    output.add(j);
+                }
+            }
+        }
+
+        return output.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
